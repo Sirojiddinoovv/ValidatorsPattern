9b4c27c6ab9f9574836292b46e5f604ff09796f7
Home-PC <davrbankit@gmail.com>
2025-07-05 10:00:16 +0500

    first commit

---
+++ src/main/kotlin/uz/nodir/validatepatterns/ValidatePatternsApplication.kt
@@ -0,0 +1,11 @@
+package uz.nodir.validatepatterns
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ValidatePatternsApplication
+
+fun main(args: Array<String>) {
+    runApplication<ValidatePatternsApplication>(*args)
+}
---
+++ src/test/kotlin/uz/nodir/validatepatterns/ValidatePatternsApplicationTests.kt
@@ -0,0 +1,13 @@
+package uz.nodir.validatepatterns
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ValidatePatternsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
